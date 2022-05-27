761f7718c6bbc0aff4242d6edda8832925cbb0ef
yalh76 <yalh@yahoo.com>
2022-05-28 01:03:07 +0200

    Apply example_ynh

---
+++ doc/DESCRIPTION.md
@@ -0,0 +1,1 @@
+Cowyo is a self-contained wiki server that makes jotting notes easy and fast. The most important feature here is simplicity. Other features include versioning, page locking, self-destructing messages, encryption, and listifying. See https://github.com/schollz/cowyo#usage
--- scripts/_common.sh
+++ scripts/_common.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 
-# Detect the system architecture to download the right tarball
-# NOTE: `uname -m` is more accurate and universal than `arch`
-# See https://en.wikipedia.org/wiki/Uname
-if [ -n "$(uname -m | grep 64)" ]; then
-	architecture="amd64"
-elif [ -n "$(uname -m | grep 86)" ]; then
-	architecture="32bit"
-elif [ -n "$(uname -m | grep arm)" ]; then
-	architecture="arm"
-else
-	ynh_die "Unable to detect your achitecture, please open a bug describing \
-        your hardware and the result of the command \"uname -m\"." 1
-fi
+#=================================================
+# COMMON VARIABLES
+#=================================================
+
+# dependencies used by the app
+pkg_dependencies=""
+
+#=================================================
+# PERSONAL HELPERS
+#=================================================
+
+#=================================================
+# EXPERIMENTAL HELPERS
+#=================================================
+
+#=================================================
+# FUTURE OFFICIAL HELPERS
+#=================================================
